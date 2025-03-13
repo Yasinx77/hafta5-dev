962d4e27335439a8f6025a25b45b7784da9bc902
Yasinx77 <aslanyasin320@gmail.com>
2025-03-14 00:48:34 +0300

    Add files via upload

---
+++ hafta5dersodevsoru4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int sayi;
+
+    cout << "bir sayi girin (0 girerek cikabilirsiniz)" << endl;
+    cin >> sayi;
+
+    while (sayi != 0) {
+
+        cout << "bir sayi girin" << endl;
+        cout << "Girdiginiz sayi: " << sayi << endl;
+        cin >> sayi;
+    }
+
+    cout << "..............................." << endl;
+    cout << "Program sonlandirildi" << endl;
+    cout << "..............................." << endl;
+
+    return 0;
+}
