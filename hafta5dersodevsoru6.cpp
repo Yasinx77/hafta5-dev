b86f9fe4c59cd4a3633a20170e981e5135b9a022
Yasinx77 <aslanyasin320@gmail.com>
2025-03-14 00:49:08 +0300

    Add files via upload

---
+++ hafta5dersodevsoru6.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string cumle;
+    char harf;
+    int sayac = 0, i = 0;
+
+    cout << "Bir cumle girin: ";
+    getline(cin, cumle); // Cümleyi al
+    cout << "Bir harf girin: ";
+    cin >> harf; // Harfi al
+
+    do {
+        if (cumle[i] == harf) {
+            sayac++; // Harf eşleşirse sayaç artır
+        }
+        i++;
+    } while (i < cumle.length()); // Döngü cümlenin sonuna kadar sürsün
+
+    cout << "Cumlenizde " << harf << " harfi " << sayac
+        << " kez geciyor, bu harf size ugur getiriyor!" << endl;
+
+    return 0;
+}
