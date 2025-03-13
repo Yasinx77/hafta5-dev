10bf546bcad141c9daaea0259607cf96108ea40b
Yasinx77 <aslanyasin320@gmail.com>
2025-03-14 00:48:13 +0300

    Add files via upload

---
+++ hafta5odevnesnesoru3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string mesaj;
+    cout << "Bir mesaj girin: ";
+    getline(cin, mesaj); // Kullanıcıdan mesaj alın
+
+    int uzunluk = mesaj.length(); // Mesajın uzunluğunu bulun
+    cout << "Mesajin uzunlugu: " << uzunluk << endl;
+
+    int i = 0;
+    while (i < uzunluk) {
+        cout << "Karakter: " << mesaj[i] << " - ASCII Degeri: " << int(mesaj[i]) << endl;
+        i++;
+    }
+
+    return 0;
+}
