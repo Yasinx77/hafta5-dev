7995bcb1752b66b6f89bbd496b39cebfb5630990
Yasinx77 <aslanyasin320@gmail.com>
2025-03-14 00:47:24 +0300

    Add files via upload

---
+++ hafta5odevderssoru2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int sicaklik;
+
+    cout << "Hava sicakligini girin (°C): ";
+    cin >> sicaklik;
+
+    if (sicaklik < 0) {
+        cout << "Kalin mont giyin!" << endl;
+    }
+    else if (sicaklik >= 0 && sicaklik < 15) {
+        cout << "Hirka yeterli olabilir." << endl;
+    }
+    else if (sicaklik >= 15 && sicaklik <= 25) {
+        cout << "Hafif bir ceket iyi olur." << endl;
+    }
+    else {
+        cout << "Tisort zamani!" << endl;
+    }
+
+    return 0;
+}
